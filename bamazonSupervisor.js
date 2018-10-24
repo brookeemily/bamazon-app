65d89af208b602d661d68c2ef26dc6dd64dcf36c
brooke <brooke.mitchell@comcast.net>
2018-10-24 00:39:22 -0400

    view products sales by department

---
+++ bamazonSupervisor.js
@@ -0,0 +1,84 @@
+// include required packages
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+var Table = require('cli-table');
+
+
+// create the connection information for the sql database
+var connection = mysql.createConnection({
+    host: "localhost",
+    
+    port: 3306,
+    
+    user: "root",
+    password: "password",
+    database: "bamazon"
+});
+
+// connect to the mysql server and sql database
+connection.connect(function(err) {
+    if (err) throw err;
+    // run the start function after the connection is made to prompt the user
+    helloSupervisor();
+});
+
+
+// If a manager selects Add to Inventory, your app should display a prompt that will let the manager "add more" of any item currently in the store.
+// If a manager selects Add New Product, it should allow the manager to add a completely new product to the store.
+
+
+// Running this application will first display all of the items available for sale...
+//Include the ids, names, and prices of products for sale.
+
+function helloSupervisor() {
+    // START INQUIRER   
+    inquirer
+    // START PROMPT
+    .prompt([
+        // WHAT ITEM DO YOU WANT?
+        {
+            name: "menu",
+            type: "list",
+            choices: ["View Products Sales by Department", "Create New Department"],
+            message: "Welcome, Bamazon Supervisor. What would you like to do?"
+        }
+    ])
+    .then(function(answer) {
+        switch (answer.menu) {
+        // If a manager selects View Products for Sale
+        case "View Products Sales by Department":
+            console.log("Here are the product sales by department....");
+            // Run view products function
+            viewSalesByDept();
+            break;
+        
+        case "Create New Department":
+        createNewDept ();
+        break;
+        }
+    });
+}
+
+
+function viewSalesByDept() { 
+    // instantiate
+    var query = "SELECT * FROM departments INNER JOIN products ON (departments.department_name = products.department_name)"
+
+    connection.query(query, function(err, results) {
+        
+    var table = new Table({
+        head: ['department_id', 'department_name', 'over_head_costs', 'product_sales', 'total_profit']
+        // table is an Array, so you can `push`, `unshift`, `splice` and friends
+    });
+            for (var i = 0; i < results.length; i++) {
+            table.push(
+                [results[i].department_id, results[i].department_name, results[i].over_head_costs, results[i].product_sales, ((results[i].product_sales) - (results[i].over_head_costs))],
+                // [results[2].department_id, results[2].department_name, results[2].over_head_costs, results[2].product_sales, ((results[2].product_sales) - (results[2].over_head_costs))],
+                // [results[3].department_id, results[3].department_name, results[3].over_head_costs, results[3].product_sales, ((results[3].product_sales) - (results[3].over_head_costs))],
+                
+            );
+        }
+            console.log(table.toString());
+        // }
+        });
+        }
--- schema.sql
+++ schema.sql
@@ -10,20 +10,40 @@ CREATE TABLE products (
     department_name VARCHAR(45) NOT NULL,
     price DECIMAL(5,2) NOT NULL,
     stock_quantity INT(5) NOT NULL,
+    product_sales DECIMAL(7, 2)  NULL,
     PRIMARY KEY (item_id)
 );
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES("toothbrush", "beauty & health", 5.39, 100);
+CREATE TABLE departments (
+    department_id INT NOT NULL AUTO_INCREMENT,
+    department_name VARCHAR(45) NOT NULL,
+    over_head_costs DECIMAL(7,2) NOT NULL,
+    PRIMARY KEY (department_id)
+);
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES("basketball", "sports & outdoors", 15.00, 40);
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES("tent", "sports & outdoors", 95.95, 20);
+INSERT INTO products (product_name, department_name, price, stock_quantity, product_sales)
+VALUES("toothbrush", "beauty & health", 5.39, 100, 10000);
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES("super rare beanie baby", "toys, kids & baby", 999.99, 2);
+INSERT INTO products (product_name, department_name, price, stock_quantity, product_sales)
+VALUES("basketball", "sports & outdoors", 15.00, 40, 1000 );
+
+INSERT INTO products (product_name, department_name, price, stock_quantity, product_sales)
+VALUES("tent", "sports & outdoors", 95.95, 20, 15000);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity, product_sales)
+VALUES("super rare beanie baby", "toys, kids & baby", 999.99, 2, 100000);
+
+INSERT INTO departments (department_name, over_head_costs)
+VALUES ("toys, kids & baby", 10000);
+
+INSERT INTO departments (department_name, over_head_costs)
+VALUES ("sports & outdoors", 5000);
+
+INSERT INTO departments (department_name, over_head_costs)
+VALUES ("beauty & health", 2000);
 
 USE bamazon;
 SELECT * FROM products;
+
+SELECT * FROM departments;
